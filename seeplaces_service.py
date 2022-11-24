0e1bdb117cf9f204c44d7cbe9379ebe0f8b0a277
Martin Hanzely <hanzely.martin@yahoo.com>
2022-11-24 17:14:23 +0100

    Added ExcursionSpokenLanguages helper methods.

--- seeplaces_service.py
+++ seeplaces_service.py
@@ -1,6 +1,105 @@
+import os
+from dataclasses import dataclass
+from typing import Any
+from urllib.parse import urljoin
+
+import requests
+
+
+class SeePlacesError(Exception):
+    """
+    Generic SeePlaces service exception.
+    """
+
+
+class ConfigurationError(SeePlacesError):
+    """
+    Service configuration error.
+    """
+
+
+class ApiConnectionError(SeePlacesError):
+    """
+    Service connection error.
+    """
+
+
+class SeePlacesOptions:
+    """
+    Options class for SeePlaces service.
+    """
+    base_url: str
+    api_version: str
+
+    def __init__(self) -> None:
+        try:
+            self.base_url = os.environ["BASE_URL"]
+            self.api_version = os.environ["API_VERSION"]
+        except KeyError as exc:
+            raise ConfigurationError(f"Missing configuration key: {exc}") from exc
+
+
 class SeePlacesService:
     """
     Connection service for SeePlaces API.
     """
-    def __init__(self) -> None:
-        raise NotImplementedError
+    _options: SeePlacesOptions
+
+    @dataclass
+    class _SpokenLanguage:
+        """
+        Internal class for handling spoken languages.
+        """
+        id: str
+        name: str
+        url_name: str
+
+        def __init__(self, Id: str, Name: str, UrlName: str) -> None:  # pylint: disable=C0103
+            self.id = Id
+            self.name = Name
+            self.url_name = UrlName
+
+    def __init__(self, options: SeePlacesOptions) -> None:
+        self._options = options
+
+    def _get_language_ids(self, languages: set[str]) -> set[str]:
+        """
+        Returns IDs of given languages.
+        """
+        api_response = self._call_excursion_spoken_languages()
+        all_languages = self._parse_languages_from_response(api_response)
+        return {_l.id for _l in all_languages if _l.name in languages}
+
+
+    def _call_excursion_spoken_languages(self) -> requests.Response:
+        """
+        Returns response of ExcursionSpokenLanguages api call.
+        """
+        base_url = self._options.base_url
+        endpoint_path = "api/Excursion/ExcursionSpokenLanguages"
+        query = {"api-version": self._options.api_version}
+        headers = {"accept": "application/json"}
+
+        response = requests.get(
+            urljoin(base_url, endpoint_path),
+            params=query,
+            headers=headers,
+            timeout=60,
+        )
+        try:
+            response.raise_for_status()  # Raise exception if response status is not OK.
+        except requests.HTTPError as exc:
+            raise ApiConnectionError(f"Cannot connect to endpoint: {endpoint_path}") from exc
+        return response
+
+    def _parse_languages_from_response(self, response: requests.Response) -> list[_SpokenLanguage]:
+        """
+        Returns api call response parsed into list of _SpokenLanguage objects.
+        """
+        json_data: dict[str, Any] = response.json()
+
+        languages = []
+        if languages_from_response := json_data.get("SpokenLanguages"):
+            for _l in languages_from_response:
+                languages.append(SeePlacesService._SpokenLanguage(**_l))
+        return languages
--- test_seeplaces_service.py
+++ test_seeplaces_service.py
@@ -1,10 +1,101 @@
-import pytest
+import os
+from collections.abc import Iterator
+from unittest import mock
 
-from seeplaces_service import SeePlacesService
+import pytest
+import requests
+
+from seeplaces_service import (
+    ConfigurationError,
+    SeePlacesOptions,
+    SeePlacesService,
+)
+
+
+@pytest.fixture
+def setup_env() -> Iterator[None]:
+    required_env = {
+        "BASE_URL": "https://api.example.com",  # Must end with trailing slash.
+        "API_VERSION": "1.0",
+    }
+    with mock.patch.dict(os.environ, required_env):
+        yield
+
+
+class TestSeePlacesOptions:
+
+    def test_init(self):
+        base_url = "https://www.example.com"
+        with mock.patch.dict(os.environ, {"BASE_URL": base_url}):
+            options = SeePlacesOptions()
+        
+        assert options.base_url == base_url
+
+    def test_init_fails(self):
+        missing_env = "BASE_URL"
+        _env = {k: v for k, v in os.environ.items()}
+        _ = dict.pop(_env, missing_env)
+        with mock.patch.dict(os.environ, _env, clear=True):
+            with pytest.raises(ConfigurationError) as exc:
+                _ = SeePlacesOptions()
+                assert missing_env in str(exc)
 
 
 class TestSeePlacesService:
 
-    def test(self):
-        with pytest.raises(NotImplementedError):
-            _ = SeePlacesService()
+    @pytest.fixture
+    def dummy_service(setup_env) -> SeePlacesService:
+        options = SeePlacesOptions()
+        return SeePlacesService(options=options)
+
+    def test__get_language_ids(self, monkeypatch, dummy_service):
+        test_id = "test_id"
+        languages = [
+            SeePlacesService._SpokenLanguage(test_id, "Slovak", "slovak"),
+            SeePlacesService._SpokenLanguage("test_id1", "Czech", "czech")
+        ]
+        monkeypatch.setattr(dummy_service, "_call_excursion_spoken_languages", lambda: None)
+        monkeypatch.setattr(dummy_service, "_parse_languages_from_response", lambda _: languages)
+        language_ids = dummy_service._get_language_ids({"Slovak"})
+        assert len(language_ids) == 1
+        assert language_ids.pop() == test_id
+
+    @pytest.mark.parametrize(
+        "status_code",
+        [
+            pytest.param(200, id="status_ok"),
+            pytest.param(404, marks=pytest.mark.xfail, id="status_not_found"),
+        ]
+    )
+    def test__call_excursion_spoken_languages(self, monkeypatch, dummy_service, status_code):
+        response = requests.Response()
+        response.status_code = status_code
+        monkeypatch.setattr(requests, "get", lambda *args, **kwargs: response)
+        _ = dummy_service._call_excursion_spoken_languages()
+
+    @pytest.mark.parametrize(
+        "json_data, expected_output",
+        [
+            pytest.param(
+                {},
+                [],
+                id="empty_response",
+            ),
+            pytest.param(
+                {"SpokenLanguages": [{"Id": "test_id", "Name": "Slovak", "UrlName": "slovak"}]},
+                [SeePlacesService._SpokenLanguage("test_id", "Slovak", "slovak")],
+                id="response_from_api_docs",
+            ),
+        ]
+    )
+    def test__parse_languages_from_response(self, dummy_service, json_data, expected_output):
+
+        class _MockResponse:
+
+            def json(self):
+                return json_data
+
+        languages = dummy_service._parse_languages_from_response(response=_MockResponse())
+        assert len(languages) == len(expected_output)
+        if len(languages) > 0:
+            assert languages[0].id == expected_output[0].id
